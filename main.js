77073951d589106c3029f564a17df9606c6aafdf
Marshall Buck <dev@marshallbuck.com>
2022-06-25 16:48:26 -0500

    delete meme works

--- main.js
+++ main.js
@@ -43,21 +43,28 @@ function appendImage(url, memeTop, memeBottom) {
   const textHolder = document.createElement('div');
   textHolder.setAttribute('class', 'text-holder text-center meme-text');
   relHolder.append(textHolder);
-
+  // Always create the top p, and if form is empty set innerText to blank
+  // this is so flex box will work correctly
   const topP = document.createElement('p');
   textHolder.append(topP);
 
   if (memeTop === '') topP.innerText = '';
   else topP.innerText = memeTop;
-
+  // ONly need to add bottom text if text ils in form
   if (memeBottom !== '') {
     const bottomP = document.createElement('p');
     bottomP.innerText = memeBottom;
     textHolder.append(bottomP);
   }
 
-
-
+  // Add button wiht event listener
+  const buttonContainer = document.createElement('div');
+  buttonContainer.setAttribute('class', 'container-center card-bottom');
+  relHolder.append(buttonContainer);
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = "Delete Me";
+  buttonContainer.append(deleteButton);
+  deleteButton.addEventListener('click', deleteMeme);
 
 
 
@@ -66,4 +73,6 @@ function appendImage(url, memeTop, memeBottom) {
 
 function deleteMeme(e) {
   console.log(e);
+  console.log(e.target.parentNode.parentNode.parentNode);
+  e.target.parentNode.parentNode.parentNode.remove();
 }
