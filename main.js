7fe670d0d1213aaecd7dec5f8bd01993565573fd
Marshall Buck <dev@marshallbuck.com>
2022-06-25 14:41:28 -0500

    ditched bootstrap, form is styled

--- main.js
+++ main.js
@@ -1,17 +1,50 @@
-const submitBtn = document.querySelector('#meme-gen-form');
+const form = document.querySelector('#meme-gen-form');
+const memeHolderRow = document.querySelector('#meme-holder');
 
 
 
 
-submitBtn.addEventListener('submit', (e) => {
+
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  appendImage();
-  console.log(e);
+  const url = form.elements.webAddress.value;
+
+  const memeTop = form.elements.memeTop.value;
+
+  const memeBottom = form.elements.memeBottom.value;
+  console.log(url, memeTop, memeBottom);
+  appendImage(url, memeTop, memeBottom);
+
+
+  form.reset();
 });
 
-function appendImage() {
-  const memeHolder = document.querySelector('#meme-holder');
+function appendImage(url, memeTop, memeBottom) {
+  const meme = document.createElement('div');
+  meme.setAttribute('class', ' col-4 position-relative');
+  memeHolderRow.append(meme);
+
+
   const img = document.createElement('img');
-  memeHolder.append(img);
-  img.setAttribute('src', 'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+  img.setAttribute('src', url);
+  img.setAttribute('class', 'img-fluid ');
+
+  meme.append(img);
+
+  const textHolder = document.createElement('div');
+  textHolder.setAttribute('class', 'position-absolute top-0 start-0 bottom-0 end-0 p-3 text-white d-flex flex-column justify-content-between align-items-center');
+  meme.append(textHolder);
+  const topText = document.createElement('p');
+  topText.setAttribute('class', 'text-wrap text-center');
+  topText.innerText = memeTop;
+  textHolder.appendChild(topText);
+  const bottomText = document.createElement('p');
+  bottomText.innerText = memeBottom;
+  bottomText.setAttribute('class', 'text-wrap text-center');
+  textHolder.appendChild(bottomText);
+
+
+
+
+
 }
